794e5703c41eefda37a9bbc7a18aed87e351877d
Arun <er.arunkumarvenugopal@gmail.com>
2023-09-27 09:45:34 +0530

    service script

--- example.groovy
+++ example.groovy
@@ -1,5 +1,16 @@
-job('example-git') {
-  steps {
-    shell('echo Hello World!')
+job('Service-Check') {
+
+description ('This job checks some services')
+  scm
+ {
+    git("https://github.com/arunkumarv11/JenkinsDSL.git", Master)
+ }
+  triggers
+ {
+   scm("* * * * *")
+ }
+  steps
+ {
+    shell('date','sudo systemctl status firewalld.service')
   }
 }
